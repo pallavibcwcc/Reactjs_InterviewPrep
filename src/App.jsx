c04f89d284f29c9fbdfb0d8fd71b90c9165c9c5c
Pallavi Kumari <pallavi.brty@gmail.com>
2025-07-25 15:29:50 +0530

    dommanipilationinreact

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Dom from './Changecircletosquare/Dom'
+
+const App =() => {
+ 
+  return(
+   <div>
+  <Dom/>
+
+    </div>
+  )
+}
+export default App;
---
+++ src/Changecircletosquare/Dom.jsx
@@ -0,0 +1,19 @@
+
+import '../Styles/Dom.css'
+
+import {useState } from 'react'
+
+const Dom =() => {
+  const[iscircle,setIscircle]=useState(true);
+  return(
+    <div className="Shape">
+      <div className={iscircle?"circle":"square"}></div>
+       
+
+          <button onClick={()=>setIscircle(!iscircle)}>changeShape</button>
+    
+
+    </div>
+  )
+}
+export default Dom;
